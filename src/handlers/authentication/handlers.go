df03daa43f9a7eb61c138b6b3796a24ea0565005
Marlo Rodrigues <marlo.98@hotmail.com.br>
2024-07-11 00:03:08 +0000

    Start login system

--- src/handlers/authentication/handlers.go
+++ src/handlers/authentication/handlers.go
@@ -11,5 +11,18 @@ func Login(c *gin.Context) {
 }
 
 func Logout(c *gin.Context) {
+	var loginData sLogin
+
+	if err := c.ShouldBindJSON(&loginData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	is_valid := validLogin(&loginData)
+
+	if is_valid != "" {
+
+	}
+
 	c.IndentedJSON(http.StatusOK, gin.H{})
 }
--- src/handlers/authentication/validators.go
+++ src/handlers/authentication/validators.go
@@ -1,1 +1,33 @@
 package authentication
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type sLogin struct {
+	Username string `validate:"required" json:"username"`
+	Password string `validate:required" json:"password"`
+}
+
+func validLogin(data *sLogin) string {
+	validate := validator.New()
+	if err := validate.Struct(data); err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+
+		return _formatValidationError(validationErrors)
+	}
+
+	return ""
+}
+
+func _formatValidationError(err validator.ValidationErrors) string {
+	var errorMsg string
+
+	for _, e := range err {
+		errorMsg += fmt.Sprintf("Campo '%s' falhou na validação com tag '%s'\n", e.Field(), e.Tag())
+	}
+
+	return errorMsg
+}
